3978ed9fc2c302998b8dd1f06574117c73ed64bf
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-27 00:36:17 -0400

    Removes extraneous comments

--- test/00_server.rb
+++ test/00_server.rb
@@ -2,21 +2,14 @@ require 'active_support/core_ext'
 require 'webrick'
 require 'rails_lite'
 
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
 server = WEBrick::HTTPServer.new :Port => 8080
 trap('INT') { server.shutdown }
 
 class MyController < ControllerBase
   def go
-    # render_content("hello world!", "text/html")
+    render_content("hello world!", "text/html")
 
-    # after you have template rendering, uncomment:
-    # render :show
-
-    # after you have sessions going, uncomment:
+    render :show
     session["count"] ||= 0
     session["count"] += 1
     render :counting_show
--- test/01_router_server.rb
+++ test/01_router_server.rb
@@ -3,10 +3,6 @@ require 'json'
 require 'webrick'
 require 'rails_lite'
 
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
 server = WEBrick::HTTPServer.new :Port => 8080
 trap('INT') { server.shutdown }
 
@@ -36,8 +32,6 @@ server.mount_proc '/' do |req, res|
   router.draw do
     get Regexp.new("^/statuses/$"), StatusController, :index
     get Regexp.new("^/users/$"), UserController, :index
-
-    # uncomment this when you get to route params
     get Regexp.new("^/statuses/(?<id>\\d+)$"), StatusController, :show
   end
 
